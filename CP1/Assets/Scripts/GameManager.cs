c30c78599731000a2a47d30690918918b29c9412
Runtomaitorun <yeahmy1022@gmail.com>
2024-06-05 16:07:33 +0100

    profile page demo

---
+++ CP1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void ShowChatBox()
+    {
+        // show the chat box!!
+        Debug.Log("chatbox I need you");
+    }
+}
---
+++ CP1/Assets/Scripts/VFXManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXManager : MonoBehaviour
+{
+    public void CuisineSteam()
+    {
+        //have some cuisine steam instantiated and destroyed
+    }
+}
