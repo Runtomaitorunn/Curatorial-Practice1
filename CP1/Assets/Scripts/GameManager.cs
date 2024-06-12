4ce9d9a835c26a1bf48fc4a0579be17314219b48
Runtomaitorun <yeahmy1022@gmail.com>
2024-06-12 14:45:38 +0100

    ChatButton and profile button work

--- CP1/Assets/Scripts/GameManager.cs
+++ CP1/Assets/Scripts/GameManager.cs
@@ -1,12 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject chat;
+    public GameObject profile;
+    public Image BG;
+    public Sprite[] sprites;
     public void ShowChatBox()
     {
         // show the chat box!!
         Debug.Log("chatbox I need you");
+    }
+    public void GoToChat()
+    {
+        chat.SetActive(true);
+
+        profile.SetActive(false);
+        BG.sprite = sprites[0];
+    }
+    public void GoToProfile()
+    {
+        chat.SetActive(false);
+
+        profile.SetActive(true);
+        BG.sprite = sprites[1];
     }
 }
